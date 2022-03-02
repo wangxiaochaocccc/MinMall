29d8af0b9e87efcd2e32216c4130e05cc376c6d7
wangchao <wangxiaochaocccc@163.com>
2022-03-02 14:24:49 +0800

    feat:接口错误拦截

--- src/main.js
+++ src/main.js
@@ -1,9 +1,28 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 
-Vue.config.productionTip = false
+Vue.config.productionTip = true
 
+// axios基础配置
+axios.defaults.baseURL = '/api'
+axios.defaults.timeout = 8000
+
+// 接口拦截
+axios.interceptors.response.use(function(response) {
+  let res = response.data
+  if(res.status == 0) {
+    return res.data
+  }else if(res.status == 10){
+    window.location.href = '/#/login'
+  }else {
+    alert(res.msg)
+  }
+})
+
+Vue.use(VueAxios,axios)
 new Vue({
   router,
   render: h => h(App)
