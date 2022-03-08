0b7d5548d1bb5c709a91f9a7215848096fac31d5
wangchao <wangxiaochaocccc@163.com>
2022-03-08 14:59:30 +0800

    feat:nav-footer

---
+++ src/components/nav-footer.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="footer">
+    <div class="footer-logo">
+      <img src="../../public/imgs/logo-footer.png" alt="" />
+      <p>小米商城</p>
+    </div>
+    <div class="footer-link">
+      <div class="link-box">
+        <a href="javascript:;">我爱我家</a>
+        <span>|</span>
+        <a href="javascript:;">我爱我家</a>
+        <span>|</span>
+        <a href="javascript:;">我爱我家</a>
+        <span>|</span>
+        <a href="javascript:;">我爱我家</a>
+      </div>
+      <div class="copyright">copyright@20220322</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+<style scoped lang="scss">
+@import './../assets/scss/config.scss';
+
+.footer {
+  height: 234px;
+  background-color: $colorB;
+  border-top: 4px solid $colorA;
+  color: #999;
+  text-align: center;
+  .footer-logo {
+    width: 64px;
+    margin: 46px auto 31px;
+    img {
+      width: 53px;
+      height: 36px;
+      margin-bottom: 13px;
+    }
+  }
+  .footer-link {
+    a {
+      display: inline-block;
+      color: #999;
+      margin-bottom: 8px;
+    }
+    span {
+      margin: 0 10px;
+    }
+  }
+}
+</style>
--- src/pages/home.vue
+++ src/pages/home.vue
@@ -2,16 +2,19 @@
   <div>
     <nav-header></nav-header>
     <router-view></router-view>
+    <nav-footer></nav-footer>
   </div>
 </template>
 
 <script>
 import NavHeader from "../components/nav-header";
+import NavFooter from "../components/nav-footer";
 
 export default {
   name: "nav-home",
   components: {
     NavHeader,
+    NavFooter
   },
 };
 </script>
