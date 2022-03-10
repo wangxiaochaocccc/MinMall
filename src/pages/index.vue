80608fa80938cb6ffa70ebb14017018a45b8ec15
wangchao <wangxiaochaocccc@163.com>
2022-03-10 11:21:03 +0800

    feat:首页swiper上悬浮tab完成

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -7,6 +7,29 @@
             <li class="menu">
               <a href="javascript:;">手记 电话卡</a>
               <span class="iconfont">&#xe62d;</span>
+              <div class="children">
+                <ul>
+                  <li
+                    v-for="(item, i) in phoneList"
+                    :key="i"
+                    class="phone-main-box"
+                  >
+                    <div class="phone-box" v-for="(phone, j) in item" :key="j">
+                      <img
+                        :src="
+                          phone && phone.img
+                            ? phone.img
+                            : '/imgs/item-box-1.png'
+                        "
+                        alt=""
+                      />
+                      <span class="name">{{
+                        phone && phone.name ? phone.name : '小米9'
+                      }}</span>
+                    </div>
+                  </li>
+                </ul>
+              </div>
             </li>
             <li class="menu">
               <a href="javascript:;">电视 盒子</a>
@@ -94,7 +117,28 @@ export default {
       }, {
         id: '46',
         url: '/imgs/slide-5.jpg'
-      }]
+      }],
+      phoneList: [
+        [
+          {
+            id: '31',
+            img: '/imgs/item-box-1.png',
+            name: '小米9'
+          }, {
+            id: '32',
+            img: '/imgs/item-box-2.png',
+            name: '小米 青春版'
+          }, {
+            id: '33',
+            img: '/imgs/item-box-3.jpg',
+            name: '红米9+'
+          }, {
+            id: '34',
+            img: '/imgs/item-box-4.jpg',
+            name: '移动4G社区'
+          }
+        ], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]
+      ]
     }
   },
   components: {
@@ -110,6 +154,7 @@ export default {
 
 .index {
   .swiper-box {
+    position: relative;
     .swiper-container {
       height: 451px;
       img {
@@ -142,6 +187,39 @@ export default {
         }
         &:hover {
           background-color: #ff6600;
+          .children {
+            display: block;
+          }
+        }
+        .children {
+          display: none;
+          position: absolute;
+          top: 0;
+          left: 264px;
+          width: 964px;
+          height: 451px;
+          background-color: #fff;
+          box-shadow: 0px 7px 6px 0px rgba(0, 0, 0, 0.11);
+          border-top: 1px solid #e5e5e5;
+          .phone-main-box {
+            @include flex();
+            padding-left: 23px;
+            height: 75px;
+            .phone-box {
+              flex: 1;
+              img {
+                width: 35px;
+                height: 42px;
+                vertical-align: middle;
+                margin-right: 15px;
+              }
+              .name {
+                font-size: 14px;
+                font-weight: bold;
+                color: #333;
+              }
+            }
+          }
         }
       }
     }
