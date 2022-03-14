60c04b767958e4967edadc0ac7acb2349d36354e
wangchao <wangxiaochaocccc@163.com>
2022-03-14 16:48:20 +0800

    feat:vuex框架搭建

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import store from './store'
 // import env from './env'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
@@ -37,5 +38,6 @@ Vue.use(VueLazyLoad, {
 })
 new Vue({
   router,
+  store,
   render: h => h(App)
 }).$mount('#app')
